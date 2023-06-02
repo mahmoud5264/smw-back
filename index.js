e880209e234bd2f3f9f865530e033bf5e68bd864
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-06-02 23:59:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cron = require('node-cron');
 // app.use(
 //   fileUpload()
 // );
-app.use(cors());
+app.use(cors('*'));
 app.use(express.json());
 const conect = async () => {
   await mongoose.connect("mongodb+srv://actional79:19991999abd@cluster0.su8lkhz.mongodb.net/");
