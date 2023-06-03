a34a8bcb305363672183e9e0e6a1082744923932
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-06-04 02:58:28 +0300

    Update formRoute.js

--- routes/formRoute.js
+++ routes/formRoute.js
@@ -15,7 +15,7 @@ router.post("/create", check.validation, upload2.single("file"), createForm);
 router.get("/", check.validation, getForms);
 router.delete("/delete/:id", check.validation, deleteForm);
 router.post("/edit/:id", check.validation, editForm);
-router.get("/:id", check.validation, getMyForms);
+router.get("/:id", getMyForms);
 
 router.post("/filter", check.validation, filter);
 
