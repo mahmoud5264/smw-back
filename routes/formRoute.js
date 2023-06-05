4766b5110e151b863275a8a48e799389be49751c
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-06-05 16:12:45 +0300

    Update formRoute.js

--- routes/formRoute.js
+++ routes/formRoute.js
@@ -9,13 +9,16 @@ const {
   getMyForms,
   getNumberOfForms,
   filter,
+  getForms2,
 } = require("../controllers/formController");
 const Form = require("../models/formModel");
 router.post("/create", check.validation, upload2.single("file"), createForm);
 router.get("/", check.validation, getForms);
+router.get("/front", check.validation, getForms2);
+
 router.delete("/delete/:id", check.validation, deleteForm);
 router.post("/edit/:id", check.validation, editForm);
-router.get("/:id", getMyForms);
+router.get("/:id", check.validation, getMyForms);
 
 router.post("/filter", check.validation, filter);
 
