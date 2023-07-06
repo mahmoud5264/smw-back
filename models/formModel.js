259fe906a1e8387dd70a080063ea0af2f365bd92
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-07-06 03:04:14 +0300

    Update formModel.js

--- models/formModel.js
+++ models/formModel.js
@@ -18,7 +18,7 @@ const formSchema = mongoose.Schema(
     file:{ type: String, default: null },
     beneficiary: { type: Boolean, default: false, trim: true },
     createdBy: {type: String, default: null },
-    note : {type:String , default:"")
+    area: { type: String, default: "", trim: true },
   },
   { timestamps: true }
 );
