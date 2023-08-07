5f5753d8e73c14f684641b4a22ec06e39cb9280b
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-08 00:48:20 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -207,8 +207,7 @@ const deleteUser = async (req, res) => {
    // if (!req.user.admin && !req.user.role.includes("setting"))
    //   return res.status(400).json("not authorized");
   try {
-   await User.deleteMany({})
-    // await User.findByIdAndDelete(req.params.id)
+     await User.findByIdAndDelete(req.params.id)
     return res.status(200).json('deleted')
   } catch (error) {
     return res.status(400).json(error)
