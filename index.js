a72f9bf98b173f5c087249abbf770c1b35579ebe
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-08 00:02:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const conect = async () => {
   // mongodb+srv://actional79:azsbzxe5F1zr0tTG@cluster0.su8lkhz.mongodb.net/
   // mongodb+srv://amrmohamed09:85v39HBvrVMrHvXC@cluster0.mjasn1l.mongodb.net/
   await mongoose.connect(
-    "mongodb+srv://amrmohamed09:85v39HBvrVMrHvXC@cluster0.mjasn1l.mongodb.net/"
+    "mongodb://admin:01024792084#code@SG-smw-59213.servers.mongodirector.com:27017/admin?ssl=true"
   );
 };
 /*
