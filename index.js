e0344d4e6e91e56f7a66415c858e313c2439f411
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-08 00:16:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const conect = async () => {
   // mongodb+srv://actional79:azsbzxe5F1zr0tTG@cluster0.su8lkhz.mongodb.net/
   // mongodb+srv://amrmohamed09:85v39HBvrVMrHvXC@cluster0.mjasn1l.mongodb.net/
   await mongoose.connect(
-    "mongodb://admin:5iiNXAlzWvb8ulQF@SG-smw-59213.servers.mongodirector.com:27017/admin?ssl=true"
+    "mongodb://admin:5iiNXAlzWvb8ulQF@SG-smw-59213.servers.mongodirector.com:27017/admin?ssl=true",{
+  ssl: true,
+  sslValidate: false
+}
   );
 };
 /*
