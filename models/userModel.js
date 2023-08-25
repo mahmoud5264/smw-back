f047775792c7c937afd6ac2b187e93c32a225a12
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-25 16:38:03 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -34,6 +34,10 @@ const userShema = mongoose.Schema({
   hidden:{
     type: Boolean,
     default: false,
+  },
+  jobTittle:{
+    type:String,
+    default: '',
   }
 });
 const User = mongoose.model("User", userShema);
