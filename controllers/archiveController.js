7f20ae9b5b0594feff8c4d87ebfc0e4ae24733f8
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-29 19:57:46 +0300

    Create archiveController.js

---
+++ controllers/archiveController.js
@@ -0,0 +1,54 @@
+const Archive = require("../models/archiveModel");
+
+const add = async (req, res) => {
+  const { region, number, bookNumber, date } = req.body;
+  console.log(req.body);
+  if (!req.user || !req.file) {
+    return res.status(400).json("حدث خطأ ما حاول مره اخري");
+  }
+  try {
+    let temp = await Archive.create({
+      image: req.file.path,
+      region,
+      number,
+      bookNumber,
+      date,
+      user: req.user,
+    });
+    console.log(temp);
+    return res.status(200).json(temp);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json(err);
+  }
+};
+
+const search = async (req, res) => {
+  const { search } = req.body;
+  console.log(req.body);
+
+  let data = await Archive.find({
+    $or: [
+      { region: { $regex: `${search}` } },
+      { bookNumber: { $regex: `${search}` } },
+      { date: { $regex: `${search}` } },
+    ],
+  }).limit(30);
+  return res.status(200).json(data);
+};
+
+const get = async (req, res) => {
+  const { id } = req.query.id;
+  if(!req.query.id) return res.status(400).json('رجاء ادخال الرفم التعريفي')
+  let data = await Archive.findById(req.query.id).populate('user');
+  return res.status(200).json(data);
+};
+const getAll = async (req, res) => {
+  const page = req.query.page ? req.query.page * 1 : 1;
+  let data = await Archive.find({})
+    .skip((page - 1) * 30)
+    .limit(30);
+  return res.status(200).json(data);
+};
+
+module.exports = { add, search, get, getAll };
