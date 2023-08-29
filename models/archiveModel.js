d2f67d4beebcd19289fd7ce10639ec221d7ea093
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-08-29 19:56:30 +0300

    Create archiveModel.js

---
+++ models/archiveModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const archiveSchema = mongoose.Schema(
+    {
+      image:{ type: String, trim: true },
+      region: { type: String, trim: true },
+      number: { type: String, trim: true },
+      bookNumber: { type: String, trim: true },
+      date: { type: String, trim: true },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    },
+    { timestamps: true }
+  );
+  
+  const Archive = mongoose.model("Archive", archiveSchema);
+  module.exports = Archive;
