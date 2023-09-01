cd3865585d4821cd6f6c68eae48d578147433c23
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-09-02 00:56:42 +0300

    Update archiveRoute.js

--- routes/archiveRoute.js
+++ routes/archiveRoute.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const upload = require("../assist/multer");
-const { add, search, get, getAll } = require("../controllers/archiveController");
+const { add, search, get, getAll, deleteArchive } = require("../controllers/archiveController");
 const check = require("../guard/authGuard");
 
 router.post('/add',upload.single("file"), check.validation, add)
 router.post('/search',check.validation, search)
 router.get('/get', check.validation , get)
 router.get('/getAll', check.validation , getAll)
+router.delete('/:id', check.validation , deleteArchive)
 module.exports = router
